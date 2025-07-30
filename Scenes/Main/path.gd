f357036d922cc6f73a8267dee45194931e6dcb38
Joseph Orawiec <143203535+Joseph-Orawiec@users.noreply.github.com>
2025-07-30 14:01:10 -0500

    basic enemy and spawner

---
+++ Scenes/Main/path.gd
@@ -0,0 +1,22 @@
+extends Path2D
+
+
+
+const bug_dictionary: Dictionary = {
+	"bug_basic": preload("res://Scenes/bugBasic/BugBasic.tscn")
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _input(event):
+	if event.is_action_pressed("debug_add_bug"):
+		var temp = bug_dictionary["bug_basic"].instantiate()
+		add_child(temp)
+		pass
