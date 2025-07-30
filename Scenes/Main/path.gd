a5d368d205a038f314c4ca2df9a3945c8a63b084
Joseph Orawiec <143203535+Joseph-Orawiec@users.noreply.github.com>
2025-07-30 16:08:23 -0500

    basic weapon functionality

--- Scenes/Main/path.gd
+++ Scenes/Main/path.gd
@@ -15,8 +15,14 @@ func _ready():
 func _process(delta):
 	pass
 	
+	
 func _input(event):
 	if event.is_action_pressed("debug_add_bug"):
 		var temp = bug_dictionary["bug_basic"].instantiate()
 		add_child(temp)
 		pass
+		
+		# the groups wil ALWAYS be ordered from closest to the end to the farthest
+		var nodes = get_tree().get_nodes_in_group("bugs")
+		for n in nodes:
+			print(n.progress_ratio)
