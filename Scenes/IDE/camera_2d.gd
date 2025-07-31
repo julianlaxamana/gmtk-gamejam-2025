b6006f394537acac3d6cf7c5631287f0ab93f868
Joseph Orawiec <143203535+Joseph-Orawiec@users.noreply.github.com>
2025-07-30 22:02:23 -0500

    zooming in

--- Scenes/IDE/camera_2d.gd
+++ Scenes/IDE/camera_2d.gd
@@ -2,6 +2,10 @@ extends Camera2D
 
 var previous_coordinates
 var is_dragging
+const pa = 1.11 # Area proportionality constant
+const zoom_min = .3
+const zoom_max = 4
+const dx = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -32,3 +36,34 @@ func _input(event):
 		
 		#update the new previous coordinates
 		previous_coordinates = event.position
+		
+	if event.is_action_pressed("zoom_in"):
+		# vector from the cursor to the center of the camera
+		var d = position - get_global_mouse_position() 
+		var zoom2 = 1
+		
+		# zoom by constant screen width growth
+		# zoom2 = (zoom * get_viewport().size.x)/(get_viewport().size.x * Vector2(1, 1) - zoom * dx)
+		
+		# new zoom amount
+		zoom2 = (zoom.x * sqrt(pa)) * Vector2(1, 1)
+		
+		# clamp value
+		zoom2 = zoom2.clamp(Vector2(1, 1) * zoom_min, Vector2(1, 1) * zoom_max)
+		
+		# using the formula i derived
+		position = get_global_mouse_position() + d * (zoom / zoom2)
+		zoom = zoom2
+	if event.is_action_pressed("zoom_out"):
+		# vector from the cursor to the center of the camera
+		var d = position - get_global_mouse_position() 
+		var zoom2 = 1
+		
+		# zoom2 = (zoom * get_viewport().size.x)/(get_viewport().size.x * Vector2(1, 1) + zoom * dx)
+			
+		# take the reciprocal instead
+		zoom2 = (zoom.x / sqrt(pa)) * Vector2(1, 1)
+		zoom2 = zoom2.clamp(Vector2(1, 1) * zoom_min, Vector2(1, 1) * zoom_max)
+		position = get_global_mouse_position() + d * (zoom / zoom2)
+		zoom = zoom2
+		
